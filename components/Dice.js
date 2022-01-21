c8b47bfe7c70802ca532ee45bfadf05cc5427b3c
Martin Molvær <42579391+martinmolvaer@users.noreply.github.com>
2022-01-21 13:14:58 +0100

    version 2.0 of my dice roll app

---
+++ components/Dice.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Dice = ({ num }) => {
+  switch (num) {
+    case 1:
+      return <img src="https://svgshare.com/i/dcC.svg" />;
+      console.log(1);
+      break;
+    case 2:
+      return <img src="https://svgshare.com/i/dbK.svg" />;
+      break;
+    case 3:
+      return <img src="https://svgshare.com/i/dc5.svg" />;
+      break;
+    case 4:
+      return <img src="https://svgshare.com/i/db1.svg" />;
+      break;
+    case 5:
+      return <img src="https://svgshare.com/i/dc6.svg" />;
+      break;
+    case 6:
+      return <img src="https://svgshare.com/i/dam.svg" />;
+  }
+
+  return <div>{num}</div>;
+};
+
+export default Dice;
